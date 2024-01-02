b79504abdb9ee3ff6a92f41c2da691ed1af8a9ba
Sven Gronauer <sven.gronauer@tum.de>
2024-01-02 15:06:34 +0100

    fixed: test script

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import torch
 import argparse
 from model import TransformerClassifier
@@ -10,6 +12,14 @@ from tqdm import tqdm
 import sklearn
 from sklearn.svm import OneClassSVM
 
+from trainer import create_ensemble_mlp
+
+def calculate_sincos_from_minutes(minutes):
+    time_value = minutes * (2. * np.pi / (60 * 24))
+    sin_t = np.sin(time_value)
+    cos_t = np.cos(time_value)
+    return sin_t, cos_t
+
 def parse():
     '''Returns args passed to the train.py script.'''
     parser = argparse.ArgumentParser()
@@ -17,23 +27,24 @@ def parse():
     # transformer parameters
     parser.add_argument('--window_size', type=int, default=48)
     parser.add_argument('--input_features', type=int, default=8)
-    parser.add_argument('--d_model', type=int, default=32)
+    parser.add_argument('--output_dim', type=int, default=2)
+    parser.add_argument('--d_model', type=int, default=64)
     parser.add_argument('--nhead', type=int, default=8)
     parser.add_argument('--nlayers', type=int, default=2)
+    parser.add_argument('--ensembles', type=int, default=5)
 
-    # num_patients - 10 for track 1, 9 for track 2
-    parser.add_argument('--num_patients', type=int, default=10)
+    # num_patients - 9 for track 1, 8 for track 2
+    parser.add_argument('--num_patients', type=int, default=9)
 
     # input paths
     parser.add_argument('--features_path', type=str, help='features to use')
     parser.add_argument('--dataset_path', type=str, help='dataset path') # to get relapse labels
-    parser.add_argument('--submission_path', type=str, help='where to save the submission files') # to get relapse labels
+    parser.add_argument('--submission_path', type=str, help='where to save the submission files', default='/var/tmp/spgc-submission') # to get relapse labels
 
     # checkpoint
-    parser.add_argument('--load_path', type=str, help='path to saved model', default='checkpoints/best.pth.tar')
-    parser.add_argument('--scaler_path', type=str, help='path to saved scaler', default='checkpoints/scaler.pkl')
+    parser.add_argument('--load_path', type=str, help='path to root directory of models', default='/var/tmp/track1_win24_l2_d64')
 
-    parser.add_argument('--device', type=str, help='device to use (cpu, cuda, cuda[number])', default='cuda')
+    parser.add_argument('--device', type=str, help='device to use (cpu, cuda, cuda[number])', default='cpu')
 
     parser.add_argument('--mode', type=str, help='val, test', default='test')
 
@@ -48,75 +59,44 @@ def main():
     args = parse()
     window_size = args.window_size
 
-            
-    columns_to_scale = ['acc_norm', 'heartRate_mean', 'rRInterval_mean', 'rRInterval_rmssd', 'rRInterval_sdnn', 'rRInterval_lombscargle_power_high']
-    data_columns = columns_to_scale + ['sin_t', 'cos_t']
+    pprint(args)
+
+    print(args.d_model)
+
+    columns_to_scale = ['acc_norm', 'gyr_norm', 'heartRate_mean', 'rRInterval_mean',
+                        'rRInterval_rmssd', 'rRInterval_sdnn', 'rRInterval_lombscargle_power_high',
+                        'steps']
+    data_columns = columns_to_scale
+    target_columns = ['sin_t', 'cos_t']
 
     device = args.device
     print('Using device', args.device)
 
-    # Model
-    model = TransformerClassifier(vars(args))
-    model.to(device)
+    # Models
+    encoders = [TransformerClassifier(vars(args)).to(device) for _ in range(args.num_patients)]
+    mlps = [create_ensemble_mlp(args) for _ in range(args.num_patients)]
 
-    # load checkpoint
-    checkpoint = torch.load(args.load_path)
-    model.load_state_dict(checkpoint)
-    model.eval()
+    with open(os.path.join(args.load_path, "train_dist_anomaly_scores.pkl"), 'rb') as f:
+        train_dist_anomaly_scores = pickle.load(f)
+
+    # dataset scalers
+    scalers = []
+
+    # load checkpoints
+    for i in range(len(encoders)):
+        pth = os.path.join(args.load_path, str(i+1))
+        checkpoint = torch.load(os.path.join(pth, 'best_encoder.pth'))
+        encoders[i].load_state_dict(checkpoint)
+        encoders[i].eval()
+        checkpoint = torch.load(os.path.join(pth, 'best_ensembles.pth'))
+        mlps[i].load_state_dict(checkpoint)
+        mlps[i].eval()
+
+        # load scaler
+        with open(os.path.join(pth, "scaler.pkl"), 'rb') as f:
+            scaler = pickle.load(f)
+            scalers.append(scaler)
     torch.set_grad_enabled(False)
-
-    # load scaler
-    with open(args.scaler_path, 'rb') as f:
-        scaler = pickle.load(f)
-
-    # first calculate the one class svms
-    print('Getting features from train distribution...')
-
-    # add a collate fn which ignores None
-    def collate_fn(batch):
-        batch = [x for x in batch if x is not None]
-        if len(batch) == 0:
-            return None
-        return torch.utils.data.dataloader.default_collate(batch)
-
-    train_dataset = PatientDataset(features_path=args.features_path, 
-                                   dataset_path=args.dataset_path,
-                                   mode='train', window_size=args.window_size)
-    
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False, num_workers=8, pin_memory=True, collate_fn=collate_fn)
-
-    all_features_train = []
-    all_labels_train = []
-
-    for batch in tqdm(train_loader):
-        if batch is None:
-            continue
-
-        x = batch['data'].to(device)
-        user_ids = batch['user_id'].to(device)
-        
-        # Forward
-        _, features = model(x)
-
-        # append features
-        all_features_train.append(features.detach().cpu())
-        all_labels_train.append(user_ids.detach().cpu())
-
-    all_features_train = torch.vstack(all_features_train).numpy()
-    all_labels_train = torch.hstack(all_labels_train).numpy()
-
-    # ----- train the outlier detection of this epoch ----- #
-    print('Training Robust Covariance...')
-    clfs = []
-
-    from sklearn.covariance import EllipticEnvelope
-    # -------- train elliptic envelopes to predict scores from classification features -------- # 
-    for subject in range(args.num_patients):
-        subject_features_train = all_features_train[all_labels_train==subject]
-
-        clf = EllipticEnvelope(support_fraction=1).fit(subject_features_train)
-        # clf = OneClassSVM().fit(subject_features_train)
-        clfs.append(clf)
 
     all_auroc = []
     all_auprc = []
@@ -130,15 +110,28 @@ def main():
         user_relapse_labels = []
         user_anomaly_scores = []
         relapse_shapes = 0
+        if patient == ".DS_Store":
+            continue
+        patient_id = int(patient[1:]) - 1
+        if patient_id >= len(encoders):
+            continue
 
         for subfolder in os.listdir(patient_dir):
             if (args.mode == 'val' and 'val' in subfolder) or (args.mode == 'test' and 'test' in subfolder):
                 subfolder_dir = os.path.join(patient_dir, subfolder)
-                file = 'features.csv'
+                file = 'features_stretched_w_steps.csv'
                 file_path = os.path.join(subfolder_dir, file)
-                df = pd.read_csv(file_path, index_col=0)
+                df = pd.read_csv(file_path)  #, index_col=0)
                 df = df.replace([np.inf, -np.inf], np.nan)
                 df = df.dropna()
+
+                try:
+                    df[target_columns]
+                except KeyError:
+                    mins = df['mins']
+                    sin_t, cos_t = calculate_sincos_from_minutes(mins)
+                    df['sin_t'] = sin_t
+                    df['cos_t'] = cos_t
             
                 relapse_df = pd.read_csv(os.path.join(args.dataset_path, patient, subfolder, 'relapses.csv'))
                 # IMPORTANT - drop last row as it was falsely added by the organizers
@@ -153,29 +146,26 @@ def main():
                 for day_index in day_indices:
                     day_data = df[df['day_index'] == day_index].copy()
 
-                    relapse_label = relapse_df[relapse_df['day_index'] == day_index]['relapse'].to_numpy()[0]
-
-                    patient_id = int(patient[1:]) - 1
+                    # relapse_label = relapse_df[relapse_df['day_index'] == day_index]['relapse'].to_numpy()[0]
 
                     if len(day_data) < args.window_size:
                         # predict zero anomaly score for days without enough data - right in the middle of the inlier/outlier of robust covariance
                         relapse_df.loc[relapse_df['day_index'] == day_index, 'anomaly_score'] = 0
                         user_anomaly_scores.append(0)
-                        user_relapse_labels.append(relapse_label)
+                        user_relapse_labels.append(0)
                         continue
                     
                     sequences = []
                     if len(day_data) == window_size:
-                        start_idx = 0
-                        sequence = day_data.iloc[start_idx:start_idx + window_size]
+                        sequence = day_data.iloc[0:window_size]
                         sequence = sequence[data_columns].copy().to_numpy()
-                        sequence[:, :-2] = scaler.transform(sequence[:, :-2])
+                        # sequence[:, :-2] = scaler.transform(sequence[:, :-2])
                         sequences.append(sequence)
                     else:
                         for start_idx in range(0, len(day_data) - window_size, window_size//3): # 1/3 overlap
                             sequence = day_data.iloc[start_idx:start_idx + window_size]
                             sequence = sequence[data_columns].copy().to_numpy()
-                            sequence[:, :-2] = scaler.transform(sequence[:, :-2])
+                            # sequence[:, :-2] = scaler.transform(sequence[:, :-2])
                             sequences.append(sequence)
                     sequence = np.stack(sequences)
                     sequence_tensor = torch.tensor(sequence, dtype=torch.float32)
@@ -183,22 +173,26 @@ def main():
 
 
                     # Forward
-                    logits, features = model(sequence_tensor.to(device))
+                    features, _ = encoders[patient_id](sequence_tensor.to(device))
+                    batched_features = features[None, :, :].repeat([args.ensembles, 1, 1])
+                    mean_preds = mlps[patient_id].forward(batched_features)
 
+                    _mean = np.mean(train_dist_anomaly_scores[patient_id])
+                    _max = np.max(train_dist_anomaly_scores[patient_id])
+                    _min = np.min(train_dist_anomaly_scores[patient_id])
 
-                    current_user_clf = clfs[patient_id]
-                    features = features.detach().cpu().numpy()
+                    average = torch.mean(mean_preds, 0)
+                    var_scores = (mean_preds - average) ** 2
+                    mean_var = torch.mean(var_scores).item()
+                    anomaly_score = (mean_var - _mean) / (_max - _min)
 
-
-                    anomaly_score = -current_user_clf.decision_function(features)
-
-                    anomaly_score = anomaly_score.mean()
+                    anomaly_score = anomaly_score.item()
 
 
                     # add this to the relapse_df
                     relapse_df.loc[relapse_df['day_index'] == day_index, 'anomaly_score'] = anomaly_score
                     user_anomaly_scores.append(anomaly_score)
-                    user_relapse_labels.append(relapse_label)
+                    user_relapse_labels.append(0)
 
                 # save subfolder in submission_path
                 os.makedirs(os.path.join(args.submission_path, patient, subfolder), exist_ok=True)
@@ -245,9 +239,6 @@ def main():
         random_auprc = sum(random_auprc)/len(random_auprc)
         total_avg = (total_auroc + total_auprc) / 2
         print(f'Total AUROC: {total_auroc:.4f}, Total AUPRC: {total_auprc:.4f}, Random AVG: {total_avg:.4f}, Random AUROC: {random_auroc:.4f}, Random AUPRC: {random_auprc:.4f}, Ideal AVG: {(random_auroc + random_auprc)/2:.4f}')
-
-
-
 
 
 if __name__ == '__main__':
